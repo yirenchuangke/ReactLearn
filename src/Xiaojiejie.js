be1deff59b0956bf4f14c8b2287336c3623d8455
LiuShuangLong <13522002566@163.com>
2020-10-18 22:21:06 +0800

    小姐姐

---
+++ src/Xiaojiejie.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import "./style//Xiaojiejie.css";
+export default class Xiaojiejie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      list: ["头部按摩", "精油推背"],
+    };
+  }
+  // 输入框变化的时候
+  inoutChange = (e) => {
+    this.setState({
+      inputValue: e.target.value,
+    });
+  };
+  // 添加服务
+  addList = () => {
+    console.log(1);
+    if (this.state.inputValue) {
+      this.setState({
+        list: [...this.state.list, this.state.inputValue],
+        inputValue: "",
+      });
+    }
+  };
+  // 删除服务
+  deleteItem = (index) => {
+    let list = this.state.list;
+    list.splice(index, 1);
+    this.setState({
+      list: list,
+    });
+  };
+  render() {
+    return (
+      <div className="warp">
+        <div>
+          <input
+            type="text"
+            value={this.state.inputValue}
+            placeholder="请输入你想需要的服务"
+            onChange={this.inoutChange}
+          />
+          <button onClick={this.addList}>增加服务</button>
+        </div>
+        {this.state.list.map((item, index) => {
+          return (
+            <div
+              key={index}
+              onClick={this.deleteItem.bind(this, index)}
+              className="list"
+            >
+              {item}------------{index}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
