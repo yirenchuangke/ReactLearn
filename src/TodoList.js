894d43264a89eaba53060272b3335820ae149802
LiuShuangLong <13522002566@163.com>
2020-10-20 23:15:48 +0800

    动画

---
+++ src/TodoList.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "antd/dist/antd.css";
+import { Input, Button, List } from "antd";
+import store from "./store/index.js";
+export default class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = store.getState();
+  }
+  render() {
+    return (
+      <div style={{ margin: "10px" }}>
+        <div>
+          <Input
+            placeholder={this.state.inputValue}
+            style={{ width: "250px", marginRight: "10px" }}
+          />
+          <Button type="primary">增加</Button>
+        </div>
+        <div style={{ margin: "10px", width: "300px" }}>
+          <List
+            bordered
+            dataSource={this.state.list}
+            renderItem={(item) => <List.Item>{item}</List.Item>}
+          />
+        </div>
+      </div>
+    );
+  }
+}
