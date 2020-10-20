099b80adc775657f5233ec9e25fc19bfe066ddd9
LiuShuangLong <13522002566@163.com>
2020-10-20 22:12:28 +0800

    css3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import './style.css'
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShow: true,
+    };
+  }
+  toToggole = () => {
+    this.setState({
+      isShow: this.state.isShow ? false : true,
+    });
+    console.log(this.state.isShow);
+  };
+  render() {
+    return (
+      <>
+        <div className={this.state.isShow ? "show" : "hide"}>Boss级人物一人创客</div>
+        <div>
+          <button onClick={this.toToggole}>召唤Boss</button>
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
